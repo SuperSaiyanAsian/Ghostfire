8c071fdc762b7f5baeaff3efb1bfbbad458e125c
Lance A. <25292225+SuperSaiyanAsian@users.noreply.github.com>
2025-04-21 09:39:59 -0700

    Add Comments

--- Ghostfire.py
+++ Ghostfire.py
@@ -2,14 +2,19 @@ import pygame
 
 pygame.init()
 
+# Game Window Dimensions
 SCREEN_WIDTH = 1080
 SCREEN_HEIGHT = 720
 
+# Game Window
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 running = True
 
+# Game Loop
 while running:
+
+    # Event Handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
